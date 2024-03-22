449b281f023e0e9bc619ee781d146e44e6b5a0e8
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-03-22 17:03:21 +0400

    Fetch and handle data

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,14 @@
-export const fetchData = async (url: string) => {
+export const fetchData = async (
+  url: string,
+  apiKey: string,
+  apiHost: string
+) => {
   const res = await fetch(url, {
     method: "GET",
+    headers: {
+      "X-RapidAPI-Key": apiKey,
+      "X-RapidAPI-Host": apiHost,
+    },
   });
   if (res.ok) {
     const data = await res.json();
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { HomeContainer } from "./style";
+import { fetchData } from "../../api";
 
-const Home = () => {
-  return <div>Home</div>;
+interface Movie {
+  rank: number;
+  title: string;
+  thumbnail: string;
+  rating: string;
+}
+
+const Home: React.FC = () => {
+  const [movieData, setMovieData] = useState<Movie[]>([]);
+  const apiKey = "844b22c324msh1e3b8a407db1434p1264f4jsnb943ac8c12f9";
+  const apiHost = "imdb-top-100-movies.p.rapidapi.com";
+  const [error, setError] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetchData(
+          "https://imdb-top-100-movies.p.rapidapi.com/top100",
+          apiKey,
+          apiHost
+        );
+        setMovieData(response);
+        console.log(response);
+      } catch (error: any) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <HomeContainer>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>Error: {error}</div>
+      ) : (
+        movieData.map((movie: Movie, index: number) => (
+          <div key={index}>
+            <h2>{movie.title}</h2>
+            <img src={movie.thumbnail} alt={movie.title} />
+            <p>Rank: {movie.rank}</p>
+            <p>Rating: {movie.rating}</p>
+          </div>
+        ))
+      )}
+    </HomeContainer>
+  );
 };
 
 export default Home;

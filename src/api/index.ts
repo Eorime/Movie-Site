109a9941bef25ad03ac452fd981f7737d8262e47
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-03-23 19:11:30 +0400

    Fetch for movie details

--- src/api/index.ts
+++ src/api/index.ts
@@ -16,3 +16,22 @@ export const fetchData = async (
   }
   throw new Error("Something went wrong :(");
 };
+
+export const fetchDataDetails = async (
+  url: string,
+  apiKey: string,
+  apiHost: string
+) => {
+  const resp = await fetch(url, {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": apiKey,
+      "X-RapidAPI-Host": apiHost,
+    },
+  });
+  if (resp.ok) {
+    const detailData = resp.json();
+    return detailData;
+  }
+  throw new Error("Something went wrong :(");
+};
--- src/pages/movieDetails/MovieDetails.tsx
+++ src/pages/movieDetails/MovieDetails.tsx
@@ -1,9 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MovieContainer, Spinner } from "./style";
 import { BarLoader } from "react-spinners";
+import { fetchDataDetails } from "../../api";
 
 const MovieDetails: React.FC = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [movieDetails, setMovieDetails] = useState<any>([]);
+  const [error, setError] = useState<String>("");
+  const apiKey = "844b22c324msh1e3b8a407db1434p1264f4jsnb943ac8c12f9";
+  const apiHost = "imdb-top-100-movies.p.rapidapi.com";
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      try {
+        const response = await fetchDataDetails(
+          "https://imdb-top-100-movies.p.rapidapi.com/top32",
+          apiKey,
+          apiHost
+        );
+        setMovieDetails(response);
+        console.log(response);
+      } catch (error) {
+        setError((error as Error).message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchMovie();
+  }, []);
+
+  console.log(movieDetails);
 
   return (
     <MovieContainer>
@@ -12,7 +39,6 @@ const MovieDetails: React.FC = () => {
           <BarLoader />
         </Spinner>
       )}
-      MovieDetails
     </MovieContainer>
   );
 };

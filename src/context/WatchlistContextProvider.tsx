64828df65d31cc95b076c6784067696d0ba77e63
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-03-30 17:40:42 +0400

    Set up watchlist context

--- src/context/WatchlistContextProvider.tsx
+++ src/context/WatchlistContextProvider.tsx
@@ -8,3 +8,31 @@ interface Movie {
   rating: string;
   description: string;
 }
+
+interface WatchlistContextType {
+  watchlist: Movie[];
+  addToWatchlist: (movie: Movie) => void;
+}
+
+const WatchlistContext = createContext<WatchlistContextType>({
+  watchlist: [],
+  addToWatchlist: () => {},
+});
+
+export const useWatchlist = () => useContext(WatchlistContext);
+
+export const WatchlistProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [watchlist, setWatchlist] = useState<Movie[]>([]);
+
+  const addToWatchlist = (movie: Movie) => {
+    setWatchlist((prev) => [...prev, movie]);
+  };
+
+  return (
+    <WatchlistContext.Provider value={{ watchlist, addToWatchlist }}>
+      {children}
+    </WatchlistContext.Provider>
+  );
+};

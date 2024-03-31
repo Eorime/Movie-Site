67607968cacdd28105a325fe466db2688230a4d8
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-03-31 16:37:01 +0400

    Implement remove from watchlist

--- src/pages/watchlist/Watchlist.tsx
+++ src/pages/watchlist/Watchlist.tsx
@@ -4,14 +4,22 @@ import { WatchlistContainer } from "./styles";
 import { useWatchlist } from "../../context/WatchlistContextProvider";
 
 const Watchlist: React.FC = () => {
-  const { watchlist } = useWatchlist();
+  const { watchlist, removeFromWatchlist } = useWatchlist();
+  const handleRemoveFromWatchlist = (rank: number) => {
+    removeFromWatchlist(rank);
+  };
   console.log(watchlist);
   return (
     <WatchlistContainer>
       <Navbar />
       <ul>
         {watchlist.map((movie) => (
-          <li key={movie.rank}>{movie.title}</li>
+          <li key={movie.rank}>
+            {movie.title}
+            <button onClick={() => handleRemoveFromWatchlist(movie.rank)}>
+              Remove
+            </button>
+          </li>
         ))}
       </ul>
       Watchlist

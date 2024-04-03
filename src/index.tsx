7802a21c739bb93893ee687959a959c754ef55d7
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-04-03 19:24:33 +0400

    Add background gradient

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { GradientContainer } from "./style";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <GradientContainer>
+    <Router>
+      <App />
+    </Router>
+  </GradientContainer>
 );
---
+++ src/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const GradientContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background: linear-gradient(to bottom right, #092e25, #0c1928);
+  font-family: Arial, Helvetica, sans-serif;
+  color: #e0e1ef;
+`;
+
+export const Content = styled.div`
+  padding: 20px;
+`;

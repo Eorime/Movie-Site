c0f6642f8a8ed720c0e8cec56de9cdf376289659
Elene Dgebuadze <elenedgebuadze83@gmail.com>
2024-04-04 15:18:41 +0400

    Minor gradient style change

--- src/style.ts
+++ src/style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const GradientContainer = styled.div`
   width: 100%;
   min-height: 100vh;
-  background: linear-gradient(to bottom right, #092e25, #0c1928);
+  background: linear-gradient(to right, #124e3f, #021224);
   font-family: Arial, Helvetica, sans-serif;
   color: #e0e1ef;
   padding-top: 25px;
